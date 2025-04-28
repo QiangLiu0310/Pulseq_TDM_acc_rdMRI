ebe27c41cf8247fa0cb7a72324a3a3fcad991e74
QiangLiu0310 <qiangliu2019@163.com>
2025-04-28 09:12:04 -0400

    rm toppe sequences

--- Pulseq_tdm_recon/Pulseq_Qiang_v142-main/matlab/demoSeq/toppe_tdm/jon/toppe_diffusion_QL/PulCeq-main/matlab/isdelayblock.m
+++
@@ -1,7 +0,0 @@
-function idb = isdelayblock(block)
-% function idb = isdelayblock(block)
-% 
-% Returns true if block contains no waveforms
-
-idb = isempty(block.rf) & isempty(block.adc) & ...
-        isempty(block.gx) & isempty(block.gy) & isempty(block.gz);
